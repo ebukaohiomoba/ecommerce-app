cb3ec5526760538df677a4790f9fa90fc80fb76d
ebukaohiomoba <chukwuebukaohiomoba@gmail.com>
2023-05-15 11:26:29 -0700

    Created logic for product counter

---
+++ script.js
@@ -0,0 +1,22 @@
+let productcount = 0;
+document.getElementById("product-count").innerText = productcount;
+
+function deleteproduct() {
+    if (productcount <= 0){
+        return false;
+    } else {
+        productcount = productcount - 1 
+        document.getElementById("product-count").innerText = productcount;
+    }
+}
+
+function addproduct() {
+    if (productcount >= 20){
+        alert("You've added the maximum number")
+    } else {
+        productcount = productcount + 1 
+        document.getElementById("product-count").innerText = productcount;
+    }
+}
+
+
