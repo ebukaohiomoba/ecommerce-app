5bf1d44ac35be2a496f5577b1bbe17140c7da60e
ebukaohiomoba <chukwuebukaohiomoba@gmail.com>
2024-07-27 14:28:20 -0700

    Fixed Server.js routing and nav bar error

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -18,6 +18,4 @@ function searchSneakers() {
             }
         })
         .catch(error => console.error('Error:', error));
-  }
-
-  
+}
--- public/scripts/server.js
+++ public/scripts/server.js
@@ -6,8 +6,23 @@ const sneaks = new SneaksAPI();
 const app = express();
 const PORT = process.env.PORT || 5001; // Changed port to 5001
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the project root directory
+app.use(express.static(path.join(__dirname, '../')));
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/index.html'));
+});
+
+// Route for the products page
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/products.html'));
+});
+
+// Route for the about page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/aboutus.html'));
+});
 
 app.get('/api/sneakers/:keyword', (req, res) => {
     const keyword = req.params.keyword;
